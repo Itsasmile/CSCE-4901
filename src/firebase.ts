1825968b5ebccb6e2f7ce536322c83524785b6af
Cloud <Itsasmilemohammed@gmail.com>
2024-11-06 00:59:30 -0600

    more stuff

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,26 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-const firebaseConfig = {
+
+interface Config {
+    apiKey: string;
+    authDomain: string;
+    databauseURL: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+}
+
+const firebaseConfig: Config = {
   apiKey: 'AIzaSyD6QqmGEEy-uTUdIUFHC38mbtuv3kGMLoU',
   authDomain: 'school-9b924.firebaseapp.com',
   databaseURL: 'https://school-9b924-default-rtdb.firebaseio.com/',
   projectId: 'school-9b924',
   storageBucket: 'school-9b924.appspot.com',
-  messagingSenderId: '612048409247'
+  messagingSenderId: '612048409247',
 };
+
+
 const app = initializeApp(firebaseConfig);
 
 // Example usage: Access Firebase services
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState, useRef, ReactNode, useContext } from "react";
-import './Home.css';
-import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
-import { signOut } from 'firebase/auth';
-import { ref, listAll, getDownloadURL } from 'firebase/storage';
-import { useNavigate } from 'react-router-dom';
-import { auth, db, storage } from './firebaseConfig'; // Import Firebase services
-import Dropdown from 'react-bootstrap/Dropdown';
-import { AuthContext } from "context/AuthContext";
+import { useEffect, useState, ReactNode, useContext } from "react";
+import "./Home.css";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../firebase"; // Import Firebase services
+import { AuthContext } from "../context/AuthContext";
 
 interface State {
-  allGames: string[];
+  allGames: Game[];
   currentPage: number;
   selectedCategory: string;
   searchTerm: string;
+}
+
+interface Game {
+  id: string;
+  name: string;
+  rating: string;
+  category: string;
+  platform: string;
+  accessibility: string;
+  description: string;
 }
 
 function Home(): ReactNode {
@@ -21,7 +29,7 @@ function Home(): ReactNode {
     allGames: [],
     currentPage: 1,
     selectedCategory: "All",
-    searchTerm: ""
+    searchTerm: "",
   });
   const gamesPerPage = 6;
   const navigate = useNavigate();
@@ -32,12 +40,18 @@ function Home(): ReactNode {
 
   const fetchGames = async () => {
     try {
-      const gamesCollection = collection(db, 'games');
+      const gamesCollection = collection(db, "games");
       const gamesSnapshot = await getDocs(gamesCollection);
-      const gamesList = gamesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      const gamesList = gamesSnapshot.docs.map(
+        (doc) =>
+        ({
+          id: doc.id,
+          ...doc.data(),
+        } as Game)
+      );
       setState({
         ...state,
-        allGames: gamesList
+        allGames: gamesList,
       });
     } catch (error) {
       console.error("Error fetching games:", error);
@@ -47,33 +61,39 @@ function Home(): ReactNode {
   const handleSearch = () => {
     const lowerCaseSearchTerm = state.searchTerm?.toLowerCase() || "";
     const selectedCategory = state.selectedCategory;
+    const filteredGames = state.allGames.filter((game) => {
+      const name = game.name.toLowerCase();
+      const category = game.category.toLowerCase();
+      const platform = game.platform.toLowerCase();
+      const accessibility = game.accessibility?.toLowerCase();
 
-    const filteredGames = state.allGames.filter(game => {
       if (selectedCategory === "All") {
-        if (!lowerCaseSearchTerm) return true;
         return (
-          game.name.toLowerCase().includes(lowerCaseSearchTerm) ||
-          game.category.toLowerCase().includes(lowerCaseSearchTerm) ||
-          game.platform.toLowerCase().includes(lowerCaseSearchTerm) ||
-          (game.accessibility && game.accessibility.toLowerCase().includes(lowerCaseSearchTerm))
+          name.includes(lowerCaseSearchTerm) ||
+          category.toLowerCase().includes(lowerCaseSearchTerm) ||
+          platform.toLowerCase().includes(lowerCaseSearchTerm) ||
+          accessibility?.toLowerCase().includes(lowerCaseSearchTerm)
         );
       }
 
       if (selectedCategory === "Name") {
-        return game.name.toLowerCase().includes(lowerCaseSearchTerm);
+        return name.toLowerCase().includes(lowerCaseSearchTerm);
       } else if (selectedCategory === "Category") {
-        return game.category.toLowerCase().includes(lowerCaseSearchTerm);
+        return category.toLowerCase().includes(lowerCaseSearchTerm);
       } else if (selectedCategory === "Platform") {
-        return game.platform.toLowerCase().includes(lowerCaseSearchTerm);
+        return platform.toLowerCase().includes(lowerCaseSearchTerm);
       } else if (selectedCategory === "Accessibility" && game.accessibility) {
-        return game.accessibility.toLowerCase().includes(lowerCaseSearchTerm);
+        return accessibility.toLowerCase().includes(lowerCaseSearchTerm);
       }
 
       return false;
     });
 
-    setGames(filteredGames);
-    setCurrentPage(1);
+    setState({
+      ...state,
+      currentPage: 1,
+      games: filteredGames
+    });
   };
 
   useEffect(() => {
@@ -84,26 +104,25 @@ function Home(): ReactNode {
     try {
       await signOut(auth);
       setUser(null);
-      navigate('/login');
+      navigate("/login");
     } catch (error) {
       console.error("Error logging out:", error);
     }
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-    document.body.classList.toggle('dark-mode', !isDarkMode);
+    document.body.classList.toggle("dark-mode", !isDarkMode);
   };
 
   function BasicExample({ selectedCategory, handleSelect }) {
     const [localShowDropdown, setLocalShowDropdown] = useState(false);
-
 
     const handleMouseEnter = () => {
       setLocalShowDropdown(true);
@@ -115,15 +134,15 @@ function Home(): ReactNode {
 
     useEffect(() => {
       const handleClickOutside = (event) => {
-        if (!event.target.closest('.dropdown')) {
+        if (!event.target.closest(".dropdown")) {
           setLocalShowDropdown(false);
         }
       };
 
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
 
       return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
+        document.removeEventListener("mousedown", handleClickOutside);
       };
     }, []);
 
@@ -137,15 +156,23 @@ function Home(): ReactNode {
           {selectedCategory}
         </Dropdown.Toggle>
         <Dropdown.Menu
-          className={`dropdown-menu ${localShowDropdown ? 'show' : ''}`}
+          className={`dropdown-menu ${localShowDropdown ? "show" : ""}`}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
           <Dropdown.Item onClick={() => handleSelect("All")}>All</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleSelect("Name")}>Name</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleSelect("Category")}>Category</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleSelect("Platform")}>Platform</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleSelect("Accessibility")}>Accessibility</Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSelect("Name")}>
+            Name
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSelect("Category")}>
+            Category
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSelect("Platform")}>
+            Platform
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSelect("Accessibility")}>
+            Accessibility
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     );
@@ -157,40 +184,94 @@ function Home(): ReactNode {
       <header className="header bg-blue-500 text-white py-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="logo">
-            <img src="https://firebasestorage.googleapis.com/v0/b/school-9b924.appspot.com/o/logowithoutbg.png?alt=media&token=7dbbe48b-289c-49b4-9102-aaab09d8b028" alt="Website Logo" className="w-12 h-12 rounded-full border border-white" />
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/school-9b924.appspot.com/o/logowithoutbg.png?alt=media&token=7dbbe48b-289c-49b4-9102-aaab09d8b028"
+              alt="Website Logo"
+              className="w-12 h-12 rounded-full border border-white"
+            />
           </div>
           <h1 className="text-3xl font-bold">CoolTeamUNT</h1>
           <nav className="flex space-x-4">
-            <a href="#" className="hover:underline">Home</a>
-            <a href="#" className="hover:underline">Software</a>
-            <a href="#" className="hover:underline">Games</a>
-            <a href="#" className="hover:underline">Movies</a>
-            <a href="#" className="hover:underline">Forum</a>
-            <a href="#" className="hover:underline">FAQs</a>
+            <a href="#" className="hover:underline">
+              Home
+            </a>
+            <a href="#" className="hover:underline">
+              Software
+            </a>
+            <a href="#" className="hover:underline">
+              Games
+            </a>
+            <a href="#" className="hover:underline">
+              Movies
+            </a>
+            <a href="#" className="hover:underline">
+              Forum
+            </a>
+            <a href="#" className="hover:underline">
+              FAQs
+            </a>
           </nav>
           <div className="flex space-x-4 items-center">
-            <button onClick={toggleDarkMode} className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200">
-              {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+            <button
+              onClick={toggleDarkMode}
+              className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200"
+            >
+              {isDarkMode ? "Light Mode" : "Dark Mode"}
             </button>
             {user ? (
               <div className="relative profile-container">
-                <div className="flex items-center space-x-2 cursor-pointer" onClick={() => setShowDropdown(!showDropdown)}>
-                  <img src={userAvatar} alt="User Avatar" className="rounded-full" style={{ width: '40px', height: '40px' }} />
+                <div
+                  className="flex items-center space-x-2 cursor-pointer"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  <img
+                    src={userAvatar}
+                    alt="User Avatar"
+                    className="rounded-full"
+                    style={{ width: "40px", height: "40px" }}
+                  />
                   <p className="font-bold">{userName || user.email}</p>
                 </div>
                 {showDropdown && (
                   <div className="profile-dropdown show">
-                    <button className="dropdown-item" onClick={() => navigate('/change-profile')}>Change Profile Picture</button>
-                    <button className="dropdown-item" onClick={() => navigate('/change-name')}>Change Name</button>
-                    <button className="dropdown-item" onClick={() => navigate('/post')}>Create Post</button>
-                    <button className="dropdown-item" onClick={handleLogout}>Log out</button>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => navigate("/change-profile")}
+                    >
+                      Change Profile Picture
+                    </button>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => navigate("/change-name")}
+                    >
+                      Change Name
+                    </button>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => navigate("/post")}
+                    >
+                      Create Post
+                    </button>
+                    <button className="dropdown-item" onClick={handleLogout}>
+                      Log out
+                    </button>
                   </div>
                 )}
               </div>
             ) : (
               <>
-                <button className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200" onClick={() => navigate('/login')}>Login</button>
-                <button className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200" onClick={() => navigate('/register')}>Register</button>
+                <button
+                  className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
               </>
             )}
           </div>
@@ -201,9 +282,14 @@ function Home(): ReactNode {
       <div className="search-section bg-blue-300 py-8">
         <div className="container mx-auto text-center">
           <h2 className="text-4xl font-bold mb-4">We share what we love</h2>
-          <p className="italic mb-6">"NOTHING IS ABSOLUTE, NOTHING IS FOREVER, NOTHING FROM NOTHING"</p>
+          <p className="italic mb-6">
+            "NOTHING IS ABSOLUTE, NOTHING IS FOREVER, NOTHING FROM NOTHING"
+          </p>
           <div className="flex justify-center items-center space-x-2">
-            <BasicExample selectedCategory={selectedCategory} handleSelect={setSelectedCategory} />
+            <BasicExample
+              selectedCategory={selectedCategory}
+              handleSelect={setSelectedCategory}
+            />
             <input
               type="text"
               value={searchTerm}
@@ -212,7 +298,12 @@ function Home(): ReactNode {
               className="w-1/2 p-2 rounded-l-lg border-none"
               placeholder="What are you looking for? (Name, Category, Platform, etc)"
             />
-            <button className="bg-blue-600 text-white p-2 rounded-r-lg" onClick={handleSearch}>Search</button>
+            <button
+              className="bg-blue-600 text-white p-2 rounded-r-lg"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
           </div>
         </div>
       </div>
@@ -220,36 +311,51 @@ function Home(): ReactNode {
       {/* Main Content Section */}
       <div className="main-content container mx-auto py-10">
         <div className="flex flex-wrap justify-between">
-          {games.slice((currentPage - 1) * gamesPerPage, currentPage * gamesPerPage).map((game) => (
-            <div key={game.id} className="w-1/3 p-4">
-              <div className="game-card bg-gray-100 p-6 rounded-lg shadow-md">
-                <img
-                  src={game.image_url}
-                  alt={game.name}
-                  className="mb-4 cursor-pointer"
-                  onClick={() => navigate(`/game/${game.id}`)}
-                  style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-                />
-                <h3 className="text-xl font-bold">{game.name}</h3>
-                <p className="text-gray-700 mb-2">Category: {game.category}</p>
-                <p className="text-gray-700 mb-2">Platform: {game.platform}</p>
-                <p className="text-gray-700 mb-2">Rating: {game.rating}</p>
-                <p className="text-gray-600">{game.description}</p>
+          {games
+            .slice((currentPage - 1) * gamesPerPage, currentPage * gamesPerPage)
+            .map((game) => (
+              <div key={game.id} className="w-1/3 p-4">
+                <div className="game-card bg-gray-100 p-6 rounded-lg shadow-md">
+                  <img
+                    src={game.image_url}
+                    alt={game.name}
+                    className="mb-4 cursor-pointer"
+                    onClick={() => navigate(`/game/${game.id}`)}
+                    style={{
+                      width: "100%",
+                      height: "200px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <h3 className="text-xl font-bold">{game.name}</h3>
+                  <p className="text-gray-700 mb-2">
+                    Category: {game.category}
+                  </p>
+                  <p className="text-gray-700 mb-2">
+                    Platform: {game.platform}
+                  </p>
+                  <p className="text-gray-700 mb-2">Rating: {game.rating}</p>
+                  <p className="text-gray-600">{game.description}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         {/* Pagination Controls */}
         <div className="pagination flex justify-center mt-8">
-          {[...Array(Math.ceil(games.length / gamesPerPage)).keys()].map(number => (
-            <button
-              key={number + 1}
-              onClick={() => setCurrentPage(number + 1)}
-              className={`px-4 py-2 mx-1 ${currentPage === number + 1 ? 'bg-blue-600 text-white' : 'bg-white text-blue-600'} rounded-lg border`}
-            >
-              {number + 1}
-            </button>
-          ))}
+          {[...Array(Math.ceil(games.length / gamesPerPage)).keys()].map(
+            (number) => (
+              <button
+                key={number + 1}
+                onClick={() => setCurrentPage(number + 1)}
+                className={`px-4 py-2 mx-1 ${currentPage === number + 1
+                  ? "bg-blue-600 text-white"
+                  : "bg-white text-blue-600"
+                  } rounded-lg border`}
+              >
+                {number + 1}
+              </button>
+            )
+          )}
         </div>
       </div>
     </div>
